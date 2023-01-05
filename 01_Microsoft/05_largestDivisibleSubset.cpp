e7e5d887d7c33cb8ffbd95a75325490f96f6d862
bishalprasad321 <bishalprasad321@gmail.com>
2023-01-05 12:37:37 +0530

    Solved Problem 05

---
+++ 01_Microsoft/05_largestDivisibleSubset.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        if(nums.size()==0)
+            return nums;
+
+        sort(nums.begin(),nums.end());
+        bool flag = true;
+
+        if(nums[0] == 1)
+            flag = false;
+        else
+        {
+            nums.push_back(1);
+            flag = true;
+        }
+
+        sort(nums.begin(),nums.end());
+
+        int i,j;
+        int dp[nums.size()][2];
+        for(i = nums.size() - 1; i >= 0; i--)
+        {
+            dp[i][0] = 0;
+            dp[i][1] = i;
+            for(j = i + 1; j < nums.size(); j++)
+            {
+                if(nums[j] % nums[i] == 0)
+                {
+                    if(dp[j][0] > dp[i][0])
+                    {
+                        dp[i][0] = dp[j][0];
+                        dp[i][1] = j;
+                    }  
+                }
+            }
+            dp[i][0]++;
+        }
+        i = 0;
+        vector<int> ans;
+        ans.push_back(nums[i]);
+
+        while(dp[i][1] != i)
+        {
+            i = dp[i][1];
+            ans.push_back(nums[i]);
+            
+        }
+        if(flag)
+            ans.erase(ans.begin());
+
+        return ans;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
